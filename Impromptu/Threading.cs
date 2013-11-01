866bc1ce7d43a76e1cbddf76b69ef43a5ff4c869
jlh <justinlhudson@gmail.com>
2013-10-31 21:06:31 -0400

    surpress exceptions by default. kinda like a one shot...

--- Impromptu/Threading.cs
+++ Impromptu/Threading.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Threading;
+using System.Diagnostics;
 
 namespace Impromptu
 {
     public static class Threading
     {
-        public static bool TryExecute(TimeSpan timeout, Action action, bool abort = true)
+        public static bool TryExecute(TimeSpan timeout, Action action, bool abort = true, bool surpressExceptions = true)
         {
-            var thread = new Thread(() => action());
-            thread.Start();
-            var completed = thread.Join(timeout);
-            if(!completed && abort)
-                thread.Abort();
-            return completed;
+            try
+            {
+                var thread = new Thread(() => action());
+                thread.Start();
+                var completed = thread.Join(timeout);
+                if(!completed && abort)
+                    thread.Abort();
+                return completed;
+            }
+            catch(Exception ex)
+            {
+                Trace.WriteLine(ex);
+                if(!surpressExceptions)
+                    throw;
+            }
+            return false;
         }
     }
 }
