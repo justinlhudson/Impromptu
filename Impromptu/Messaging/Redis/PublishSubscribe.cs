76d31b94990c28cf113a6195c4bd722bcd9e1baa
jlh <>
2014-03-02 16:49:13 -0500

    Redis Pub/Sub tested. Send Generic types and allows for publishes to sender (making extra client).

--- Impromptu/Messaging/Redis/PublishSubscribe.cs
+++ Impromptu/Messaging/Redis/PublishSubscribe.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using ServiceStack.Redis;
 using Impromptu.Utilities;
+using System.Threading;
+using System.Collections.Generic;
 
-namespace Impromptu.Messaging.Redis.PublishSubscribe
+namespace Impromptu.Messaging.Redis
 {
-    public class PublishSubscribe
+    public class PublishSubscribe : IDisposable
     {
         public delegate void Message(string channel, object value);
 
         public event Message OnMessage;
 
-        private readonly RedisClient _client;
+        private readonly List<RedisClient> _client = new List<RedisClient>();
         private readonly IRedisSubscription _subscription;
+        private Thread _blocking;
 
-        public PublishSubscribe(string host = "localhost", int port = 6379, string password = null, long db = 0) // defaults
+        public PublishSubscribe(string host = "localhost", int port = 6379, string password = null, long db = 0, bool publishToSelf = true) // defaults
         {
-            _client = new RedisClient(host, port, password, db);
-            _subscription = _client.CreateSubscription();
+            _client.Add(new RedisClient(host, port, password, db));
+            _subscription = _client[0].CreateSubscription();
+
+            if(publishToSelf)
+                _client.Add(new RedisClient(host, port, password, db));
+
+            _subscription.OnSubscribe = channel =>
+            {
+                ;
+            };
+            _subscription.OnUnSubscribe = channel =>
+            {
+                ;
+            };
 
             _subscription.OnMessage = (channel, message) =>
             {
@@ -28,23 +43,47 @@ namespace Impromptu.Messaging.Redis.PublishSubscribe
             };                               
         }
 
-        void Publish(string channel, object value)
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+        #endregion
+
+        public void Publish(string channel, object value)
         {
             var temp = Serializer.String.Serialize<object>(value);
-            _client.PublishMessage(channel, temp);
+            if(_client.Count > 1)
+                _client[1].PublishMessage(channel, temp);
+            else
+                _client[0].PublishMessage(channel, temp);
         }
 
-        void Subscribe(string[] channels)
+        public void Subscribe(string[] channels)
         {
-            _subscription.SubscribeToChannels(channels);
+            UnSubscribe();
+
+            if(_blocking != null && _blocking.IsAlive)
+                _blocking.Abort();
+
+            _blocking = new Thread(new ThreadStart(() => _subscription.SubscribeToChannels(channels)));
+            _blocking.Start();                
         }
 
-        void UnSubscribe(string[] channels = null)
+        public void UnSubscribe(string[] channels = null)
         {
             if(channels != null)
                 _subscription.UnSubscribeFromChannels(channels);
             else
                 _subscription.UnSubscribeFromAllChannels();
+        }
+
+        public void Close()
+        {
+            if(_blocking != null && _blocking.IsAlive)
+                _blocking.Abort();
         }
     }
 }
