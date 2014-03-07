619014e75eb617fdbee80f3c7d5368379567130f
jlh <>
2014-03-07 18:18:01 -0500

    * Dockerfile: base dockerfile as template
    * base.sh: script to make easier to run docker images
    
    * mono-3.sh: script for installing latest mono-3
    
    * setup.sh: script to easily add steps during docker build
    
    * Impromptu.csproj:
    * sources.list: 
    
    * supervisord.conf: runs applications and keeps docker up/active

---
+++ Impromptu/Docker/base/mono-3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+# Tools to compile mono
+apt-get install -f -y mono-complete
+apt-get install -f -y build-dep mono
+apt-get install -f -y libgtk-3-dev
+
+apt-get install -f -y automake autoconf libtool git g++
+
+# corlib version 96, found 110
+#aptitude reinstall libmono-corlib2.0-cil
+
+# Structure for source
+mkdir -p /opt/mono-3
+cd /opt/mono-3
+
+# Download source(s)
+git clone git://github.com/mono/libgdiplus.git
+git clone git://github.com/mono/mono.git
+git clone git://github.com/mono/xsp.git
+
+# Compile
+cd /opt/mono-3/libgdiplus
+git fetch
+./autogen.sh --prefix=/usr/local
+make
+make install
+
+cd /opt/mono-3/mono/
+git fetch
+./autogen.sh --prefix=/usr/local
+make
+make install
+
+cd /opt/mono-3/xsp
+git fetch
+./autogen.sh --prefix=/usr/local
+make
+make install
