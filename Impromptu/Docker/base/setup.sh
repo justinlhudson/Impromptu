1b2e2bd3d2bc0434a8b37f33bc8dfc0d1420bf3a
jlh <>
2014-03-07 22:03:17 -0500

    *better base is simply ssh * development has mono-3 redis mongo

--- Impromptu/Docker/base/setup.sh
+++ Impromptu/Docker/base/setup.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# Install Redis-Server
-apt-get -y install redis-server
-
-# Install MongoDB
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
-apt-get update
-apt-get -y install mongodb-10gen
-rm /var/lib/mongodb/mongod.lock
-
-# Install mono-latest
-sh ./tmp/mono-3.sh
-
 exit 0
