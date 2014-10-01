9957f845c4acd67aef9bb3ed36c4f41e16866d49
Justin L Hudson <justinlhudson@gmail.com>
2014-09-30 20:36:11 -0400

    adding stack exchange redid client, since service stack license bull.  Needs testing...

--- C#/Impromptu/Messaging/Redis/Queue.cs
+++ C#/Impromptu/Messaging/Redis/Queue.cs
@@ -1,42 +1,43 @@
-﻿/*
-using System;
-using ServiceStack.Redis;
+﻿using System;
+using StackExchange.Redis;
 using Impromptu.Utilities;
 
 namespace Impromptu.Messaging.Redis
 {
   public class Queue : RedisBase, IQueue
   {
-    public Queue(string host = "localhost", int port = 6379, string password = null, long db = 0) : base(host, port, password, db) // defaults
-    {                          
+    private IDatabase _database;
+
+    public Queue(string host = "localhost", int port = 6379, string password = null, long db = 0)
+      : base(host, port, password, db) // defaults
+    {      
+      _database = RedisClient.GetDatabase((int)Convert.ToInt64(db));                    
     }
 
     public void Flush(string list)
     {
-      while(Pop<object>(list) != null)
+      while (Pop<object>(list) != null)
         ;
     }
 
     public long Length(string list)
     {
-      var temp = RedisClient.LLen(list);
+      var temp = _database.ListLength(list);
       return  temp;
     }
 
     public void Push<T>(string list, T value)
     {
       var temp = Serializer.Bytes.Serialize<T>(value);
-      RedisClient.RPush(list, temp);
+      _database.ListRightPush(list, temp);
     }
 
     public T Pop<T>(string list)
     {
-      var temp = RedisClient.RPop(list);
-      if(temp == null || temp.Length <= 0)
+      var temp = _database.ListRightPop(list);
+      if (!temp.HasValue)
         return default(T);
       return Serializer.Bytes.Deserialize<T>(temp);
     }
   }
 }
-
-*/
--- C#/Impromptu/Messaging/Redis/RedisBase.cs
+++ C#/Impromptu/Messaging/Redis/RedisBase.cs
@@ -1,37 +1,34 @@
-﻿/*
-using System;
-using ServiceStack.Redis;
+﻿using System;
+using StackExchange.Redis;
 
 namespace Impromptu.Messaging.Redis
 {
-    public class RedisBase
+  public class RedisBase
+  {
+    private readonly ConnectionMultiplexer _client;
+
+    public ConnectionMultiplexer RedisClient { get { return _client; } }
+
+    public RedisBase(string host = "localhost", int port = 6379, string password = null, long db = 0) // defaults
     {
-        private readonly RedisClient _client;
+      //password
+      if (host.Contains(","))
+      {
+        var split = host.Split(new[]{ ',' }, 2);
+        host = split[0];
+        password = split[1];
+      }
 
-        public RedisClient RedisClient { get { return _client; } }
-
-        public RedisBase(string host = "localhost", int port = 6379, string password = null, long db = 0) // defaults
-        {
-            //password
-            if(host.Contains(","))
-            {
-                var split = host.Split(new[]{ ',' }, 2);
-                host = split[0];
-                password = split[1];
-            }
-
-            // port
-            if(host.Contains(":"))
-            {
-                // should throw if messed up so no checking done here
-                var split = host.Split(new[]{ ':' });
-                host = split[0];
-                port = (int)Convert.ToInt64(split[1]);
-            }
+      // port
+      if (host.Contains(":"))
+      {
+        // should throw if messed up so no checking done here
+        var split = host.Split(new[]{ ':' });
+        host = split[0];
+        port = (int)Convert.ToInt64(split[1]);
+      }
                 
-            _client = new RedisClient(host, port, password, db);                          
-        }
+      _client = ConnectionMultiplexer.Connect(host + ":" + port);                        
     }
+  }
 }
-
-*/
