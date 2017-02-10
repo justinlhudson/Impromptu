694e807e68e687b5091d810411739e60a55ff8a7
Justin L Hudson <justinlhudson@gmail.com>
2017-02-10 07:23:55 -0500

    cleaner look default thread options

--- C#/Impromptu/Utilities/Threading.cs
+++ C#/Impromptu/Utilities/Threading.cs
@@ -6,38 +6,39 @@ using System.Configuration;
 
 namespace Impromptu.Utilities
 {
-  public static class Threading
-  {
-    public static ParallelOptions ParallelOptionsDefault()
-    {
-      var maxDegreeOfParallelism = System.Math.Max(1, (int)Convert.ToInt16(Environment.ProcessorCount * .5));
+	public static class Threading
+	{
+		public static ParallelOptions ParallelOptionsDefault()
+		{
+			var maxDegreeOfParallelism = (int)Convert.ToInt16((Environment.ProcessorCount > 1 ? Environment.ProcessorCount / 2 : 1));
 
-      var overrideMaxDegreeOfParallelism = ConfigurationManager.AppSettings["MaxDegreeOfParallelism"];
-      if (!string.IsNullOrEmpty(overrideMaxDegreeOfParallelism))
-        maxDegreeOfParallelism = (int)Convert.ToInt64(overrideMaxDegreeOfParallelism);
+			var overrideMaxDegreeOfParallelism = ConfigurationManager.AppSettings["MaxDegreeOfParallelism"];
+			if (!string.IsNullOrEmpty(overrideMaxDegreeOfParallelism))
+				maxDegreeOfParallelism = (int)Convert.ToInt64(overrideMaxDegreeOfParallelism);
 
-      return new ParallelOptions()
-            { MaxDegreeOfParallelism = maxDegreeOfParallelism };
-    }
+			return new ParallelOptions()
+			{ MaxDegreeOfParallelism = maxDegreeOfParallelism };
+		}
 
-    public static bool TryExecute(TimeSpan timeout, Action action, bool abort = true, bool surpressExceptions = false)
-    {
-      try
-      {
-        var thread = new Thread(() => action());
-        thread.Start();
-        var completed = thread.Join(timeout);
-        if (!completed && abort)
-          thread.Abort();
-        return completed;
-      } catch (Exception ex)
-      {
-        Trace.WriteLine(ex);
-        if (!surpressExceptions)
-          throw;
-      }
-      return false;
-    }
-  }
+		public static bool TryExecute(TimeSpan timeout, Action action, bool abort = true, bool surpressExceptions = false)
+		{
+			try
+			{
+				var thread = new Thread(() => action());
+				thread.Start();
+				var completed = thread.Join(timeout);
+				if (!completed && abort)
+					thread.Abort();
+				return completed;
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(ex);
+				if (!surpressExceptions)
+					throw;
+			}
+			return false;
+		}
+	}
 }
 
