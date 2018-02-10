c88f41895ecdf3483434809ae8cadf2c1b53d09b
Justin L Hudson <justinlhudson@gmail.com>
2018-02-10 14:41:10 -0500

    Inherited SerialPort because "DataReceived" event was not firing with Ardunio.  Furthermore, wanted more subscribers to event, thus passing what is in receive buffer in event to for everyone.
    No joke took a few hours to figure out what was wrong, then smart way to handle (this smart as i could come up with)

---
+++ C#/Impromptu/IO/SerialPort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading.Tasks;
+
+namespace Impromptu.IO
+{
+    public class SerialPort : System.IO.Ports.SerialPort, IDisposable
+    {
+
+        public new event EventHandler<string> DataReceived;
+
+        public SerialPort(string portName, int baudRate, Parity parity= Parity.None, int dataBits=8, StopBits stopBits=StopBits.One) : base(portName,baudRate,parity,dataBits,stopBits)
+        {
+            this.Open();
+
+            this.DiscardOutBuffer();
+            this.DiscardInBuffer();
+
+            Task.Run(() => DataCheck());
+        }
+
+        private void DataCheck()
+        {
+            while (true)
+            {
+                if (this.IsOpen && this.BytesToRead > 0)
+                    DataReceived(this, this.ReadExisting());
+                else
+                    System.Threading.Thread.Sleep(25); // min.
+            }
+        }
+
+        #region IDisposable implementation
+
+        void IDisposable.Dispose()
+        {
+            if(this.IsOpen)
+              this.Close();
+        }
+
+        #endregion
+    }
+}
